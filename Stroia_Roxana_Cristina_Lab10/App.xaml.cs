69c14886a298b81c4e6b43eb32a9f1f4febe2277
Stroia Roxana <stroia_roxana_cristina@yahoo.com>
2020-12-08 17:21:22 +0200

    Add project files.

---
+++ Stroia_Roxana_Cristina_Lab10/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Stroia_Roxana_Cristina_Lab10.Models;
+using System.IO;
+using Stroia_Roxana_Cristina_Lab10.Data;
+
+namespace Stroia_Roxana_Cristina_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database; 
+        public static ShoppingListDatabase Database 
+        {
+            get 
+            { 
+                if (database == null) 
+                { 
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3")); 
+                } 
+                return database; 
+            } 
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Stroia_Roxana_Cristina_Lab10/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Stroia_Roxana_Cristina_Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
